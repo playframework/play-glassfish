5c628d3bfeb360392877a5a2c793507ab65aa291
Guillaume Bort <guillaume.bort@gmail.com>
2010-10-01 11:31:20 +0200

    Fix injection of Play config

--- container/src/main/java/org/glassfish/play/extension/PlayConfigCommand.java
+++ container/src/main/java/org/glassfish/play/extension/PlayConfigCommand.java
@@ -1,33 +1,58 @@
 package org.glassfish.play.extension;
 
 import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.glassfish.api.Param;
 import org.glassfish.api.admin.AdminCommand;
 import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.config.ConfigParser;
 import org.jvnet.hk2.annotations.Inject;
 import org.jvnet.hk2.annotations.Service;
+import org.jvnet.hk2.component.Habitat;
 import org.jvnet.hk2.config.ConfigSupport;
 import org.jvnet.hk2.config.SingleConfigCode;
 import org.jvnet.hk2.config.TransactionFailure;
 
-@Service(name="play-config")
+@Service(name = "play-config")
 public class PlayConfigCommand implements AdminCommand {
 
     @Param
     String frameworkPath;
+    
+    @Inject(optional = true)
+    PlayContainerConfig config;
 
     @Inject
-    PlayContainerConfig config;
+    ConfigParser configParser;
+
+    @Inject
+    Habitat habitat;
 
     public void execute(AdminCommandContext adminCommandContext) {
         try {
+            if (config == null) {
+                URL url = this.getClass().getClassLoader().getResource("init.xml");
+                if (url != null) {
+                    try {
+                        config = configParser.parseContainerConfig(habitat, url, PlayContainerConfig.class);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    Logger.getAnonymousLogger().log(Level.SEVERE, "Cannot init Play container configuration");
+                }
+            }
             ConfigSupport.apply(new SingleConfigCode<PlayContainerConfig>() {
+
                 public Object run(PlayContainerConfig playContainerConfig) throws PropertyVetoException, TransactionFailure {
                     playContainerConfig.setFrameworkPath(frameworkPath);
                     return null;
                 }
             }, config);
-        } catch(TransactionFailure e) {
+        } catch (TransactionFailure e) {
         }
     }
 }
